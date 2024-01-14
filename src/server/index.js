5a69783170f435ac66abce4f0ca56b6275608611
Nehal Bhandari <nehalvbhandari@gmail.com>
2024-01-14 19:01:12 +0530

    chore: backend files deleted

--- src/server/index.js
+++
@@ -1,9 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-const port = 3002;
-
-import app from "./app.js";
-
-app.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Listening on PORT ${port} => http://localhost:${port}`);
-});
